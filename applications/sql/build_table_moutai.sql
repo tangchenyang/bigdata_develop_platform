83a40871956fbecd9b5112bef7fa43d8690a1004
twadmin <twadmin@twadmins-Mac-mini-2.local>
2020-10-27 20:24:38 +0800

    fix create stmt of moutai

--- applications/sql/build_table_moutai.sql
+++ applications/sql/build_table_moutai.sql
@@ -12,7 +12,6 @@ create table ods.moutai(
     trading_amount      string  comment "成交金额",
     up_down_amount      string  comment "涨跌金额",
     up_down_ratio       string  comment "涨跌比例",
-
     contraction         string  comment "缩",
     high_low_ratio      string  comment "高低差比例",
     SH_Shanghai         string  comment "SH上证",
@@ -23,5 +22,3 @@ lines terminated by '\n'
 stored as textfile;
 
 load data local inpath '/root/bigdata_develop_platform/applications/data/moutai.csv' into table ods.moutai;
-
-10/26/2020|1,619.00|1,662.92|1,600.00|1,643.00|6,601,600|-2,062,992,888|-72.33|-4.22%||3.93%|/|/
