632aa381916842cbd058970bbc40b4a60b113ff5
twadmin <twadmin@twadmins-Mac-mini-2.local>
2020-10-27 15:48:24 +0800

    fix create_table_moutai.sql

--- applications/sql/create_table_moutai.sql
+++ applications/sql/create_table_moutai.sql
@@ -1,14 +1,14 @@
 create table ods.moutai(
-    trade_date          string  commit "日期",
-    opening             string  commit "开盘",
-    highest             string  commit "最高",
-    lowest              string  commit "最低",
-    closing             string  commit "收盘",
-    trading_volume      string  commit "成交金额",
-    up_down_amount      string  commit "涨跌金额",
-    up_down_ratio       string  commit "涨跌比例",
-    contraction         string  commit "缩",
-    high_low_ratio      string  commit "高低差比例",
-    SH_Shanghai         string  commit "SH上证",
-    SH                  string  commit "sh"
+    trade_date          string  comment "日期",
+    opening             string  comment "开盘",
+    highest             string  comment "最高",
+    lowest              string  comment "最低",
+    closing             string  comment "收盘",
+    trading_volume      string  comment "成交金额",
+    up_down_amount      string  comment "涨跌金额",
+    up_down_ratio       string  comment "涨跌比例",
+    contraction         string  comment "缩",
+    high_low_ratio      string  comment "高低差比例",
+    SH_Shanghai         string  comment "SH上证",
+    SH                  string  comment "sh"
 );
