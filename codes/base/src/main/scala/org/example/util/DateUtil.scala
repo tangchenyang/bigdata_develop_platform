7f9fa197dac48ff6a0cea2ff25369bb6d3296001
tangchenyang <gteeice@163.com>
2021-01-25 12:47:26 +0800

    add module frame and example.

---
+++ codes/base/src/main/scala/org/example/util/DateUtil.scala
@@ -0,0 +1,7 @@
+package org.example.util
+
+object DateUtil {
+  def getCurrentDate() = {
+    System.currentTimeMillis()
+  }
+}
---
+++ codes/example/src/main/scala/Test.scala
@@ -0,0 +1,7 @@
+import org.example.util.DateUtil
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(DateUtil.getCurrentDate())
+  }
+}
