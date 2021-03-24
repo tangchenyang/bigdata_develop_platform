42ab3f51d50c643c956f8358d82cf76067967f38
tangchenyang <gteeice@163.com>
2021-03-24 13:12:00 +0800

    [@chenyang] #N/A test store git password

--- applications/example/moutai/run_moutai.sh
+++ applications/example/moutai/run_moutai.sh
@@ -6,3 +6,4 @@ jarPath=$APPLICATIONS_HOME/lib
 spark-submit --master yarn --deploy-mode client \
              --jars $jarPath/base-1.0-SNAPSHOT.jar \
              --class org.example.moutai.dw.fact.DwFactMoutai jars/example-1.0-SNAPSHOT.jar
+
