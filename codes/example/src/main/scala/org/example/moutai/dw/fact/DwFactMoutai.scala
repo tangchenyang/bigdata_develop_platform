ed228779fd4156c5cd7a7037ebf75142e85f587f
tangchenyang <gteeice@163.com>
2021-03-24 13:44:11 +0800

    [@chenyang] #27 N/A simplify

--- codes/example/src/main/scala/org/example/moutai/dw/fact/DwFactMoutai.scala
+++ codes/example/src/main/scala/org/example/moutai/dw/fact/DwFactMoutai.scala
@@ -6,19 +6,6 @@ import org.example.util.spark.dataframe.DataFrameReader
 object DwFactMoutai extends FactJob {
 
   override def process(): Unit = {
-    spark.sql("show databases").show
-
-    spark.sql("use example")
-    spark.sql("show tables").show
-    try{
-      spark.sql("select * from example.ods_moutai").show
-    } catch {
-      case exception: Exception => System.err.println(exception.getMessage)
-    }
-
-    spark.table("example.ods_moutai").show
-
-
     val odsDf = DataFrameReader(spark).fromHive.table("example.ods_moutai")
     odsDf.show()
   }
