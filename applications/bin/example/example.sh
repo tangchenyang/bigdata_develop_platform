c6e355f340eb41c475674dca23325ded29237efe
tangchenyang <gteeice@163.com>
2021-03-24 12:31:14 +0800

    [@chenyang] #N/A remove old moutai example.

--- applications/bin/example/example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-spark-sql -f example_build_table.sql --hivevar app_home=${APPLICATIONS_HOME}
-
-
--- applications/bin/example/sqoop/sqoop_list_tables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sqoop list-tables \
---connect jdbc:mysql://hadoop:3306/test_db?characterEncoding=UTF-8 \
---username root \
---password '123456'
