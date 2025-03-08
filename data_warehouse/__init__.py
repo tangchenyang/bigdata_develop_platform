151038eb695e2fabc411d55a7110a7734004cfdb
Chenyang Tang <44256449+tangchenyang@users.noreply.github.com>
2025-03-08 15:42:46 +0800

    [N/A] feature: set logging format

--- data_warehouse/__init__.py
+++ data_warehouse/__init__.py
@@ -3,12 +3,19 @@ import logging
 
 def init_logging():
     print(f"Initializing logging, level: INFO")
-    logging.basicConfig(
-        format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
-        level=logging.INFO,
-        datefmt="%Y-%m-%dT%H:%M:%S%z",
-    )
-    logging.root.setLevel("INFO") # todo global logger
+    # logging.basicConfig(
+    #     format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
+    #     level=logging.INFO,
+    #     datefmt="%Y-%m-%dT%H:%M:%S%z",
+    # )
+    # 设置 root logger 的格式
+    root = logging.getLogger()
+    formatter = logging.Formatter("[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    root.handlers.clear()
+    root.addHandler(handler)
+    root.setLevel(logging.INFO)
 
 
 init_logging()
